9422c3b3728fea2841aba35a03d167b179b22cb6
maijakuu <maija_kuusela@hotmail.com>
2026-03-26 11:00:33 +0200

    Edited front page, edited overall css, added logoasset

--- Software/M8Find/src/App.jsx
+++ Software/M8Find/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
+import logo from './assets/logo.PNG'
 
 function Home() {
   return (
     <>
       <section id="center">
+        
         <div>
+          <img src={logo} alt="Logo" />
           <h1>M8Find</h1>
-          <p>Welcome to M8Find!</p>
+          <p>Welcome to M8Find!</p> 
+            <p><button className="button">Start</button></p>
+            <p><button className="button">About</button></p>
         </div>
+
       </section>
     </>
   )

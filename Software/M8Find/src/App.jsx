6dd576846f4e2dfab3acf75d1e2c97e981ce65e4
maijakuu <maijakuu@edu.lapinamk.fi>
2026-03-26 16:28:47 +0200

    More layout and responsivity edits

--- Software/M8Find/src/App.jsx
+++ Software/M8Find/src/App.jsx
@@ -4,15 +4,16 @@ import Button from "./components/button";
 
 function Home() {
   return (
-    <>
-        <div className='landingpagebox'>
-          <img src={logo} alt="Logo" className='img' />
-          <h1>M8Find</h1>
-          <p><h2>Welcome to M8Find!</h2></p> 
-            <Button>Start</Button>
-            <Button>About</Button>
+    <div id="center">
+      <div className="landingpagebox">
+        <img src={logo} alt="Logo" className="img" />
+        <h1>M8Find</h1>
+        <h2>Welcome to M8Find!</h2>
+          <Button>Start</Button>
+          <Button>About</Button>
         </div>
-    </>
+      </div>
   )
 }
+
 export default Home

83cdeb3180a59128df1d1f32996dce099db682d6
Saara <saara.iltanen@hotmail.fi>
2026-03-31 15:30:26 +0300

    Modified the about page .jsx and .css. Made a back button from about page to home page

--- Software/M8Find/src/pages/About.jsx
+++ Software/M8Find/src/pages/About.jsx
@@ -1,8 +1,15 @@
+import './About.css'
+import Button from '../components/button'
+import { useNavigate } from 'react-router-dom'
+
 function About() {
+  const navigate = useNavigate()
+
   return (
-    <div>
+    <div className ="about-container">
       <h1>About Page</h1>
       <p>Welcome to the About page!</p>
+      <Button onClick={() => navigate('/')}>Back to Home</Button>
     </div>
   )
 }

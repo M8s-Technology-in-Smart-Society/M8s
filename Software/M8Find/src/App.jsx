2718e9530f37ac13a3e34ee6a71a2106406ef2ad
Saara <saara.iltanen@hotmail.fi>
2026-03-31 15:24:07 +0300

    Added pages folder and put about page there

--- Software/M8Find/src/App.jsx
+++ Software/M8Find/src/App.jsx
@@ -1,19 +1,32 @@
 import './App.css'
 import logo from './assets/logo.PNG'
-import Button from "./components/button";
+import Button from "./components/button"
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import About from './pages/About'
 
 function Home() {
+  const navigate = useNavigate()
+
   return (
     <div id="center">
       <div className="landingpagebox">
         <img src={logo} alt="Logo" className="img" />
         <h1>M8Find</h1>
         <h2>Welcome to M8Find!</h2>
-          <Button>Start</Button>
-          <Button>About</Button>
-        </div>
+        <Button>Start</Button>
+        <Button onClick={() => navigate('/about')}>About</Button>
       </div>
+    </div>
   )
 }
 
-export default Home
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  )
+}
+
+export default App

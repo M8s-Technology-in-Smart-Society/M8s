4be4edfd919a0fb10a93ae95951cf844a5fa3b6a
Saara <saara.iltanen@hotmail.fi>
2026-04-02 10:54:49 +0300

    Added information to about page, changed the font sizes and margins.

--- Software/M8Find/src/pages/About.jsx
+++ Software/M8Find/src/pages/About.jsx
@@ -7,8 +7,10 @@ function About() {
 
   return (
     <div className ="about-container">
-      <h1>About Page</h1>
-      <p>Welcome to the About page!</p>
+      <h2>About</h2>
+      <p>This app was made part of a BIP project</p>
+      <h2>M8's:</h2>
+      <p>Milan, Tony, Anatolia, Tugce, Maija and Saara</p>
       <Button onClick={() => navigate('/')}>Back to Home</Button>
     </div>
   )

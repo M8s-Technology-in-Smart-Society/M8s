ea39ee379faf5e6043a8faaa1513c40a993ff5a8
maijakuu <maija_kuusela@hotmail.com>
2026-05-09 19:32:31 +0300

    Separated Home-page from the app.jsx

--- Software/M8Find/src/App.jsx
+++ Software/M8Find/src/App.jsx
@@ -1,34 +1,16 @@
 import './App.css'
-import logo from './assets/logo.PNG'
-import Button from "./components/button"
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import About from './pages/About'
 import Start from './pages/Start'
 import Connect from './pages/Connect'
 import WebGL from './pages/WebGL'
-
-function Home() {
-  const navigate = useNavigate()
-
-  return (
-    <div id="center">
-      <div className="landingpagebox">
-        <img src={logo} alt="Logo" className="img" />
-        <h1>M8Find</h1>
-        <h2>Welcome to M8Find!</h2>
-          <div className="buttonGroup">
-            <Button onClick={() => navigate('/start')}>Start</Button>
-            <Button onClick={() => navigate('/about')}>About</Button>
-          </div>
-      </div>
-    </div>
-  ) 
-}
+import Home from './pages/Home'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/start" element={<Start />} />
       <Route path="/about" element={<About />} />
       <Route path="/connect" element={<Connect />} />
---
+++ Software/M8Find/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import '../App.css'
+import logo from '../assets/logo.PNG'
+import Button from "../components/button"
+import { useNavigate } from 'react-router-dom'
+
+function Home() {
+  const navigate = useNavigate()
+
+  return (
+    <div id="center">
+      <div className="landingpagebox">
+        <img src={logo} alt="Logo" className="img" />
+        <h1>M8Find</h1>
+        <h2>Welcome to M8Find!</h2>
+          <div className="buttonGroup">
+            <Button onClick={() => navigate('/start')}>Start</Button>
+            <Button onClick={() => navigate('/about')}>About</Button>
+          </div>
+      </div>
+    </div>
+  ) 
+}
+
+export default Home

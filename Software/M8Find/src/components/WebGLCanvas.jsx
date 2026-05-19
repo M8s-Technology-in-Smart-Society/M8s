584be2c87d2de6d05da5cc1a7cfcf6cc28ad466e
maijakuu <maija_kuusela@hotmail.com>
2026-05-19 11:53:09 +0300

    background fixed on the renderpage

--- Software/M8Find/src/components/WebGLCanvas.jsx
+++ Software/M8Find/src/components/WebGLCanvas.jsx
@@ -1,33 +1,18 @@
-import '../App.css'
-import Button from '../components/button'
-import { useNavigate } from 'react-router-dom'
-import { Canvas } from '@react-three/fiber'
-import { Children } from 'react';
+import "../App.css";
+import { Canvas } from "@react-three/fiber";
+import WebGL_UI from "./WebGL_UI";
 
-export default function WebGLCanvas({children}) {
-  const navigate = useNavigate();
-//Tänne väripallerot etäisyyden mukaan
+export default function WebGLCanvas({ children }) {
   return (
     <div className="webgl_page">
       <div className="canvas_layer">
-        <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
-        <color attach="background" args={["black"]} />
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[5, 5, 5]} intensity={1.5} />
-        {children}
+        <Canvas camera={{ position: [0, 0, 12], fov: 50 }}>
+          <color attach="background" args={["black"]} />
+          <ambientLight intensity={0.8} />
+          <directionalLight position={[5, 5, 5]} intensity={1.5} />
+          {children}
         </Canvas>
       </div>
-   </div>
+    </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
--- Software/M8Find/src/pages/WebGL.jsx
+++ Software/M8Find/src/pages/WebGL.jsx
@@ -1,18 +1,19 @@
-
-
-import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import DetectionTracker from "../components/DetectionTracker";
-import { useSensor } from "../context/SensorContext";
-import WebGL_UI from "../components/WebGL_UI";
 import WebGLCanvas from "../components/WebGLCanvas";
+import WebGL_UI from "../components/WebGL_UI";
+
 export default function WebGLScene() {
   return (
-    <Canvas camera={{ position: [0, 0, 12], fov: 50 }}>
-      <ambientLight intensity={0.8} />
-      <pointLight position={[10, 10, 10]} intensity={1.2} />
-      <DetectionTracker />
-      <OrbitControls />
-    </Canvas>
+    <div className="webgl_page">
+      <div className="canvas_layer">
+        <WebGLCanvas>
+          <DetectionTracker />
+          <OrbitControls />
+        </WebGLCanvas>
+      </div>
+
+      <WebGL_UI />
+    </div>
   );
 }
